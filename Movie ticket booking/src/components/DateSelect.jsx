1e536d4e9b136ec76f22c9e22045db76e2690318
malikusman66 <usmannawaz0304@gmail.com>
2025-07-30 20:53:46 +0500

    MOD - Date Select

--- Movie ticket booking/src/components/DateSelect.jsx
+++ Movie ticket booking/src/components/DateSelect.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import BlueCircle from "./BlurCircle";
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const DateSelect = ({ dateTime, id = "dateSelect" }) => {
+  const navigate = useNavigate();
+
+  const [selected, setSelected] = useState(null);
+
+  const onBookHandler = () => {
+    if (selected) {
+      return toast("Please select a date first!");
+    }
+    navigate(`/movies/${id}/${selected}`);
+    scrollTo(0, 0);
+  };
+
   return (
     <div id={id} className="pt-30">
       <div className="flex flex-col md:flex-row items-center justify-center gap-10 relative p-8 bg-primary/10 border border-primary/20 rounded-lg">
@@ -14,8 +28,13 @@ const DateSelect = ({ dateTime, id = "dateSelect" }) => {
           <span className="grid grid-cols-3 md:flex flex-wrap md:max-w-lg gap-4">
             {Object.keys(dateTime).map((date) => (
               <button
+                onClick={() => setSelected(date)}
                 key={date}
-                className="flex flex-col items-center justify-center h-14 w-14 aspect-square rounded cursor-pointer"
+                className={`flex flex-col items-center justify-center h-14 w-14 aspect-square rounded cursor-pointer ${
+                  selected === date
+                    ? "bg-primary text-white"
+                    : "border border-primary/70"
+                }`}
               >
                 <span>{new Date(date).getDate()}</span>
                 <span>
@@ -28,10 +47,13 @@ const DateSelect = ({ dateTime, id = "dateSelect" }) => {
           </span>
           <ChevronRightIcon width={28} />
         </div>
+        <button
+          onClick={onBookHandler}
+          className="bg-primary text-white px-8 py-2 mt-6 rounded hover:bg-primary/90 transition-all cursor-pointer"
+        >
+          Book Now
+        </button>
       </div>
-      <button className="bg-primary text-white px-8 py-2 mt-6 rounded hover:bg-primary/90 transition-all cursor-pointer">
-        Book Now
-      </button>
     </div>
   );
 };
