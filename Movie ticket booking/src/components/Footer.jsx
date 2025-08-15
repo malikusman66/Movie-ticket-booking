5a9762a195a9dda4287e6d88fd04255b2b02ec04
malikusman66 <usmannawaz0304@gmail.com>
2025-08-15 21:39:54 +0500

    mod - footer

--- Movie ticket booking/src/components/Footer.jsx
+++ Movie ticket booking/src/components/Footer.jsx
@@ -42,7 +42,7 @@ const Footer = () => {
           <div>
             <h2 className="font-semibold mb-5">Get in touch</h2>
             <div className="text-sm space-y-2">
-              <p>+1-234-567-890</p>
+              <p>+1-234-567-789</p>
               <p>contact@example.com</p>
             </div>
           </div>
